1293a182e0d0b4714b46ffa0d21264052ff325f1
kai <carame311005@gmail.com>
2023-12-26 14:27:35 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import "./App.css";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { ExploreTopBooks } from "./layouts/HomePage/ExploreTopBooks";
 import { Carousel } from "./layouts/HomePage/Carousel";
+import { Heros } from "./layouts/HomePage/Heros";
+import { LibraryServices } from "./layouts/HomePage/LibraryServices";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Navbar />
       <ExploreTopBooks />
       <Carousel />
+      <Heros />
+      <LibraryServices />
+      <Footer />
     </div>
   );
 }
