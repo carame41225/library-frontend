fd1f684235fac3e597b2ffd72569e732ddeb5b5d
kai <carame311005@gmail.com>
2023-12-27 15:53:36 +0800

    feat: Route

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,23 @@ import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { Homepage } from "./layouts/HomePage/HomePage";
 import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Navbar />
-      {/*<Homepage />*/}
-      <SearchBooksPage />
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/home' />
+        </Route>
+        <Route path='/home'>
+          <Homepage />
+        </Route>
+        <Route path='/search'>
+          <SearchBooksPage />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
